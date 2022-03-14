47742ba8e596a79e3cb5e6ab6d63c8bce012e855
chenkeyu <2574049767@qq.com>
2022-03-14 11:45:51 +0800

    加入杂质检测算法和canny算法

--- Service/hkDetect.py
+++ Service/hkDetect.py
@@ -3,7 +3,7 @@
 from Service.models import *  # set ONNX_EXPORT in models.py
 from project.datasets import *
 from project.utils import *
-
+BLUR_KERNEL_SIZE = 5
 class hkDetect:
     def __init__(self,opt=None):
         self.opt = opt
@@ -70,7 +70,7 @@ class hkDetect:
 
     def detect(self,image0,opt=None):
         image = letterbox(image0, new_shape=416)[0]
-        defect_class=''
+        defect_class=[]
         confidence=0
         # Convert
         image = image[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
@@ -105,7 +105,7 @@ class hkDetect:
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
                     s += '%g %ss, ' % (n, self.names[int(c)])  # add to string
-                    defect_class = self.names[int(c)]
+                    defect_class.append(self.names[int(c)])
 
 
             # Write results画边界框和标签
@@ -113,15 +113,83 @@ class hkDetect:
 
                 for *xyxy, conf, cls in det:
                         # if(self.names[int(cls)] is "good" and  conf<self.opt.confidence):
-
-
                         label = '%s %.2f' % (self.names[int(cls)], conf)    # 索引值对应的类别，置信度
                         xt,yt = plot_one_box(xyxy, im0, label=label, color=self.colors[int(cls)])
                         confidence = conf.item()
                         xy = xyxy[0].item()
+
+        # 杂质检测
+        if (self.zazhi_detect(image0)):
+            defect_class.append("杂质")
+
+        # 粘底检测
+        if(self.canny_detect(image0)):
+            defect_class.append("粘底")
+
         return im0,defect_class
 
 
+    def zazhi_detect(self,image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        bilateralImage = cv2.bilateralFilter(gray, BLUR_KERNEL_SIZE, 100, 15)
+        p, q = gray.shape
+        img = np.zeros((p, q), np.uint8)
+        circles1 = cv2.HoughCircles(bilateralImage, cv2.HOUGH_GRADIENT, 1, 60, param1=80, param2=40,
+                                    minRadius=120, maxRadius=200)
+        if circles1 is None:
+            pass
+        else:
+            circles = circles1[0, :, :]
+            circles = np.uint16(np.around(circles))
+            for circle in circles[:]:
+                cv2.circle(img, (circle[0], circle[1]), circle[2] - 5, 255, -1)
+
+        w, h = gray.shape
+        lst2 = []
+        for m in range(w):
+            for n in range(h):
+                if 230 >= gray[m, n] >= 1:
+                    lst2.append(gray[m, n])
+        b = np.median(lst2)
+        # print(lst2)
+        # print(b)
+        if b < 110:
+            ret, binary = cv2.threshold(gray, b - 45, 255, cv2.THRESH_BINARY_INV)
+        else:
+            ret, binary = cv2.threshold(gray, b - 40, 255, cv2.THRESH_BINARY_INV)
+        binary = cv2.bitwise_and(binary, binary, mask=img)
+        lst1 = []
+        contours, hierarchy = cv2.findContours(binary, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        for i, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            lst1.append(area)
+            x, y, w, h = cv2.boundingRect(contour)
+
+        if 55 >= sum(lst1) >= 5:
+            return True
+        else:
+            return False
+
+
+    def canny_detect(self,img):
+        img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+        img_gauss = cv2.GaussianBlur(img_gray, (3, 3), 0)
+        img_canny = cv2.Canny(img_gauss, 250, 20, 3)
+        n = len(img_canny[img_canny == 255])  # 统计图片白点数量
+        if n > 4500:  # 白色像素值大于4500则判断为缺陷
+           return True
+        else:
+            return False
+        # print(len(img_canny[img_canny == 255]))
+
+# test
+# if __name__ == '__main__':
+#     img_path = "/Users/chenkeyu/Desktop/quexian2/杂质/0002.jpg"
+#     image = cv2.imread(img_path)
+#     if(hkDetect().canny_detect(image)):
+#         print("杂质")
+#     else:
+#         print("没问题")
 
 
 
--- UI/Detection/Detection.py
+++ UI/Detection/Detection.py
@@ -1,20 +1,21 @@
-import threading
-
-from PyQt5 import QtGui
-from PyQt5.QtGui import QStandardItem
-
-from Service.Serial import COM, com
-from UI.Detection.DetectionWin import DetectionWin
-from UI.Ticket.Ticket import Ticket
-from utils.CommonHelper import CommonHelper
-import numpy as np
-from model.Opt import Opt
-from model.Camera import Camera
-from  MvCameraControl_class import *
+import sys
 import time
-import matplotlib.pyplot as plt
-# winfun_ctype = WINFUNCTYPE
+import os
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtGui import QIcon, QPixmap, QStandardItem
+from UI.Detection.D2 import DetectionWin
+from UI.Setting.Setting import Setting
+from utils.CommonHelper import CommonHelper
+from hkDetect import hkDetect
+import numpy as np
+import cv2
+from Opt import Opt
+from utils.Camera import Camera
+
+from  MvCameraControl_class import *
+winfun_ctype = WINFUNCTYPE
 DETECTION_THRESHOLD = 4000
+OUT_PATH = "../../output/Image/"
 
 class Detection(DetectionWin):
     deviceList = MV_CC_DEVICE_INFO_LIST()
@@ -27,9 +28,6 @@ class Detection(DetectionWin):
         style = CommonHelper.readQss(styleFile)
         self.setStyleSheet(style)
         self.cams={}
-        self.Ticket_flag = False
-        self.Ticket = Ticket()
-        self.Ticket.ticket_signal.connect(self.ticket_info_show)
         self.configuration = configuration
         self.g_bExit = False
         self.cameraDetectionLabels = {"1":self.camera1_detection_label,"2":self.camera2_detection_label,"3":self.camera3_detection_label,"4":self.camera4_detection_label}
@@ -37,108 +35,64 @@ class Detection(DetectionWin):
         self.GreenButton.clicked.connect(self.detect)
         self.RedButton.clicked.connect(self.stop)
         self.BlueButton.clicked.connect(self.reset)
-        self.grayButton.clicked.connect(lambda : self.Ticket.show())
-
-
-
 
     def detect(self):
-        if(not self.Ticket_flag):
-            self.Ticket.show()
-        else:
-            # threading.Thread(target=self.speed_update).start()
-            self.grayButton.setEnabled(False)
-            opt = Opt()
-            opt.cfg = self.configuration.value('CFG_PATH')
-            opt.output = self.configuration.value('SAVE_IMG_PATH')
-            opt.weights = self.configuration.value('WEIGHTS_PATH')
-            for camNo in self.configuration.value('CAM_LIST'):
-                cam = Camera(camNo,opt,1)
-                cam.show_picture_signal.connect(self.image_show)
-                cam.show_detect_info.connect(self.show_detect_info)
-                self.cams.update({camNo: cam})
-                cam.openCam()
-            self.grayButton.setEnabled(False)
-            self.grayButton.setEnabled(False)
+        self.opt = Opt()
+        self.opt.cfg = self.setting.cfg_path
+        self.opt.output = self.setting.save_img_path
+        self.opt.weights = self.setting.weights_path
+        self.hkDetect = hkDetect(self.opt)
+        for camNo in self.configuration.value('CAM_LIST'):
+            cam = Camera(camNo,self.work_thread,self.opt)
+            self.cams.update({camNo: cam})
+            cam.openCam()
+
+
 
     def stop(self):
-        self.grayButton.setEnabled(True)
-        self.Ticket_flag = False
-        # self.com.close()
-        detect_total = 0
-        bad_total =0
-        good_total=0
-        defectStatistic = {}
-        for no in self.cams:
-            detect_total += self.cams[no].detect_num
-            bad_total += self.cams[no].bad_num
-            good_total += self.cams[no].good_num
-            for key,value in self.cams[no].defectStatistic.items():
-                if key in defectStatistic:
-                    defectStatistic[key] += value
-                else:
-                    defectStatistic[key] = value
-            self.cams[no].g_bExit = True
-            self.cams[no].closeCam()
-        # self.generate_report(detect_total,bad_total,good_total,defectStatistic)
-
-
-    def generate_report(self,detect_total,bad_total,good_total,defectStatistic):
-        if(bad_total == 0):
-            return
-        plt.figure()
-        plt.rcParams["font.family"] = "kaiti"
-        plt.suptitle("检测报告", fontsize=20)
-        plt.subplot(121)
-        labels = ["检测数", "良品数", "瑕疵数"]
-        data = [detect_total, good_total, bad_total]
-        plt.title("检测表")
-        plt.bar(range(len(data)), data, tick_label=labels)
-
-        ratio = []
-        defect_class = []
-        dist = []
-        for key, value in defectStatistic.items():
-            defect_class.append(key)
-            ratio.append(int(value / bad_total * 100))
-            dist.append(0)
-        plt.subplot(122)
-
-        plt.title("各类缺陷占比", size=20)
-        plt.pie(ratio, labels=defect_class, autopct="%.1f%%", explode=dist, shadow=True)
-        plt.savefig("result-"+self.product_id_label.text()+'.jpg')
-        plt.show()
-
+        for key in self.cams:
+            self.cams[key].g_bExit = True
+            self.cams[key].closeCam()
 
     def reset(self):
         self.speedValue = 0.0
         self.ratio = 0.0
         self.detect_num = 0
         self.good_num = 0
+        self.hkDetect = None
         self.detection_number_value.setText(str(self.detect_num) + "个")
         self.speed_value.setText(str(self.speed) + "个/分钟")
         self.ratio_value.setText(str(self.ratio) + "%")
 
-    def image_show(self,result_image,defect_type,camNo):
-            if("good" in defect_type ):
-                self.cameraResultLabels[camNo].setStyleSheet("color:green;font-size:20px")
+
+    def image_show(self,image,camNo):
+        camera = self.cams[camNo]
+        image = cv2.resize(image, (self.opt.width, self.opt.height))
+        originalshow = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        area = self.hkDetect.ostu(originalshow)
+        if(area>DETECTION_THRESHOLD):
+            camera.imgs.append(originalshow)
+        elif(len(camera.imgs)):
+            camera.detect_num+=1
+            select_img = camera.imgs[int(len(camera.imgs)/2)]
+            result_image,defect_type = self.hkDetect.detect(select_img, self.opt)
+            if("ok" in defect_type ):
+                camera.good_num+=1
+                self.cameraResultLabels[camNo].setStyleSheet("color:green;font-size:14px")
             else:
-                self.cameraResultLabels[camNo].setStyleSheet("color:red;font-size:20px")
-            result_image = QtGui.QImage(result_image.data.tobytes(), result_image.shape[1], result_image.shape[0],
+                 camera.bad_num+=1
+                 self.cameraResultLabels[camNo].setStyleSheet("color:red;font-size:14px")
+            result_image = QtGui.QImage(result_image.data, result_image.shape[1], result_image.shape[0],
                                     QtGui.QImage.Format_RGB888)  # 把读取到的视频数据变成QImage形式
             self.cameraDetectionLabels[camNo].setPixmap(QtGui.QPixmap.fromImage(result_image))
-            self.cameraDetectionLabels[camNo].setScaledContents(True)
             self.cameraResultLabels[camNo].setText(defect_type)
+            row = int(camNo)-1
+            self.model.setItem(row,1, QStandardItem(str(camera.detect_num)))
+            self.model.setItem(row,2, QStandardItem(str(camera.good_num)))
+            self.model.setItem(row,3, QStandardItem(str(camera.bad_num)))
+            self.model.setItem(row,4, QStandardItem(str((camera.bad_num/camera.detect_num)*100)+"%"))
+            camera.imgs.clear()
 
-
-    def show_detect_info(self,nums,camNo):
-        camera = self.cams[camNo]
-        row = int(camNo) - 1
-        self.model.setItem(row, 1, QStandardItem(str(nums[0])))
-        self.model.setItem(row, 2, QStandardItem(str(nums[1])))
-        self.model.setItem(row, 3, QStandardItem(str(nums[2])))
-        self.model.setItem(row, 4, QStandardItem(str(int(nums[2] / nums[0]) * 100) + "%"))
-        
 
     def work_thread(self, cam=0, pData=0, nDataSize=0, camNo=0):
         stFrameInfo = MV_FRAME_OUT_INFO_EX()
@@ -146,6 +100,7 @@ class Detection(DetectionWin):
         while True:
             ret = cam.MV_CC_GetOneFrameTimeout(pData, nDataSize, stFrameInfo, 1000)
             if ret == 0:
+
                 print("get one frame: Width[%d], Height[%d], nFrameNum[%d]" % (
                     stFrameInfo.nWidth, stFrameInfo.nHeight, stFrameInfo.nFrameNum))
                 image = np.asarray(pData).reshape((stFrameInfo.nHeight, stFrameInfo.nWidth))
@@ -154,21 +109,5 @@ class Detection(DetectionWin):
                 print("no data[0x%x]" % ret)
             if self.cams[camNo].g_bExit == True:
                 break
-
-
-    def ticket_info_show(self,productName,productId):
-        self.Ticket_flag = True
-        self.product_name_label.setText(productName)
-        self.product_id_label.setText(productId)
-        local_times = time.localtime(time.time())
-        self.detect_time_label.setText(time.strftime("%Y-%m-%d %H:%M",local_times))
-
-    # def speed_update(self):
-    #     while True:
-    #         speed= com.get_data(50)
-    #         if(speed!=""):
-    #             speed = float(speed)/10
-    #             self.speed_label.setText(str(speed)+" cm/s")
-
 
 
